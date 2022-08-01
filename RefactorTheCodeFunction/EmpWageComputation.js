9db7c1b83677b96dbb51a1ade8aa4d6854a72865
Luv sahu <lksahu404@gmail.com>
2022-08-02 03:24:24 +0530

    [Luv SAHU] Calculating Refctor the Code to implement the function

---
+++ RefactorTheCodeFunction/EmpWageComputation.js
@@ -0,0 +1,53 @@
+////////UC1-Check employee present or not////////////
+const IS_ABSENT = 0
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) 
+{
+    console.log("UC1 Employee is Absent");
+    return;
+ } 
+ else
+  {
+     console.log("UC1 Employee is PRESENT")
+  }
+//////////////////UC2-to check parttime with full and absent//////////////////
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HRS=4;
+const FULL_TIME_HRS=8;
+const WAGE_PER_HOURS=20;
+let empHrs=0;
+empCheck= Math.floor(Math.random() * 10) % 3;
+switch(empCheck)
+{
+    case IS_PART_TIME:
+        empHrs=PART_TIME_HRS;
+        break;
+        case IS_FULL_TIME:
+            empHrs=FULL_TIME_HRS;
+            break;
+            default:
+                empHrs=0;
+
+}
+let empWage=empHrs*WAGE_PER_HOURS;
+console.log("UC2 empwage:"+empWage);
+
+///////////UC-3refactor the code to write a fuctiion to get work hours/////////////
+function getWorkingHours(empCheck)
+{
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HRS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HRS;
+                default:
+                    return 0;
+    }
+}
+empHrs=0;
+empCheck=Math.floor(Math.random() * 10) % 3;
+empHrs=getWorkingHours(empCheck);
+empWage=empHrs * WAGE_PER_HOURS;
+console.log("EMPWAGE: "+empWage);
